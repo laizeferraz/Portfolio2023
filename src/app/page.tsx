0b28170373b66d9b741a4df6bf5270b248c876a8
Laize Ferraz <laizeferraz@gmail.com>
2023-05-28 16:16:52 +1000

    add banner content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Animation } from '@/components/Animation'
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="mt-10">
+      <Animation />
+      <p className="mt-20 p-5">
+        👋 Hey I&apos;m Laize, a design-focused engineer based in Sydney.
+        I&apos;m currently looking for a new oppportunity to build the
+        next-generation of web based banking tools.
+      </p>
+
+      <Link href="/profile" className="mt-8">
+        View my profile →
+      </Link>
+    </div>
+  )
 }
