8861a1fb51d7791048a6246ddcd2fd4aabb9d2db
Laize Ferraz <laizeferraz@gmail.com>
2023-05-28 16:16:22 +1000

    add header to all pages and config fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import { ReactNode } from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Tenor_Sans as Tenor } from 'next/font/google'
+import { Header } from '@/components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const tenorSans = Tenor({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-logo',
+})
 
 export const metadata = {
   title: 'Laize Ferraz',
@@ -12,7 +18,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.variable} ${tenorSans.variable} container mx-auto font-primary `}
+      >
+        <main className="container mx-auto px-5">
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
