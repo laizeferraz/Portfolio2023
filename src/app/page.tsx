28040cedac2e3952117c82f06660abc138235082
Laize Ferraz <laizeferraz@gmail.com>
2023-06-02 21:25:50 +1000

    Add project list section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 import { Animation } from '@/components/Animation'
+import ProjectsList from '@/components/ProjectsList'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="mt-10">
-      <Animation />
-      <p className="mt-20 p-5">
-        👋 Hey I&apos;m Laize, a design-focused engineer based in Sydney.
-        I&apos;m currently looking for a new oppportunity to build the
-        next-generation of web based banking tools.
-      </p>
-
-      <Link href="/profile" className="mt-8">
-        View my profile →
-      </Link>
-    </div>
+    <>
+      <div className="mt-12">
+        <Animation />
+        <div className="-mt-[350px] md:-mt-20 lg:-mt-[350px]">
+          <Link href="/profile">View my profile →</Link>
+        </div>
+      </div>
+      <div className="mt-10">
+        <h2 className="">Projects</h2>
+        <ProjectsList />
+      </div>
+    </>
   )
 }
