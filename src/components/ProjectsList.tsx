8b8b3cef53cb2fa3a100c63d779d5a171c814b65
Laize Ferraz <laizeferraz@gmail.com>
2023-06-12 20:09:03 +1000

    add projects list real content

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,73 +1,26 @@
 import Image from 'next/image'
-import Letmeask from '../assets/Letmeask.png'
 import Link from 'next/link'
+import projectList from '../../data/projectList.json'
 
 export default function Home() {
   return (
     <div className="md:grid md:grid-cols-2 md:gap-4">
-      <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <Image
-          src={Letmeask}
-          alt="Letmeask Cover"
-          className="rounded object-cover"
-        />
-        <div>
-          <h4>Letmeask - 2020</h4>
-          <p className="mb-4 line-clamp-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            repellendus esse necessitatibus sint repudiandae debitis magnam ea
-            quaerat natus explicabo consequatur, soluta non voluptate recusandae
-            voluptates sapiente nobis harum odit? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Officiis repellendus esse
-            necessitatibus sint repudiandae debitis magnam ea quaerat natus
-            explicabo consequatur, soluta non voluptate recusandae voluptates
-            sapiente nobis harum odit?
-          </p>
-          <Link href="/profile">Read more →</Link>
-        </div>
-      </div>
-      <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <Image
-          src={Letmeask}
-          alt="Letmeask Cover"
-          className="rounded object-cover"
-        />
-        <div>
-          <h4>Letmeask - 2020</h4>
-          <p className="mb-4 line-clamp-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            repellendus esse necessitatibus sint repudiandae debitis magnam ea
-            quaerat natus explicabo consequatur, soluta non voluptate recusandae
-            voluptates sapiente nobis harum odit? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Officiis repellendus esse
-            necessitatibus sint repudiandae debitis magnam ea quaerat natus
-            explicabo consequatur, soluta non voluptate recusandae voluptates
-            sapiente nobis harum odit?
-          </p>
-          <Link href="/profile">Read More →</Link>
-        </div>
-      </div>
-      <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <Image
-          src={Letmeask}
-          alt="Letmeask Cover"
-          className="rounded object-cover"
-        />
-        <div>
-          <h4>Letmeask - 2020</h4>
-          <p className="mb-4 line-clamp-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            repellendus esse necessitatibus sint repudiandae debitis magnam ea
-            quaerat natus explicabo consequatur, soluta non voluptate recusandae
-            voluptates sapiente nobis harum odit? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Officiis repellendus esse
-            necessitatibus sint repudiandae debitis magnam ea quaerat natus
-            explicabo consequatur, soluta non voluptate recusandae voluptates
-            sapiente nobis harum odit?
-          </p>
-          <Link href="/profile">Read more →</Link>
-        </div>
-      </div>
+      {projectList.map((project) => (
+          <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2" key={project.id}>
+            <Image
+              src={project.ImageIntro}
+              alt={project.Title}
+              width={400}
+              height={400}
+              className="rounded object-cover"
+            />
+            <div>
+              <p className="mb-4 line-clamp-3">{project.Intro}</p>
+              <Link href="/profile">Read more →</Link>
+            </div>
+          </div>
+      ))}
     </div>
   )
 }
+
