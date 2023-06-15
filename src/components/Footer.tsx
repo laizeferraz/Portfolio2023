43cec3bc75b4d5247f0ab036e23c5f1bf9fa61e5
Laize Ferraz <laizeferraz@gmail.com>
2023-06-15 18:13:38 +1000

    create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    return (
+    <footer className="mt-5 w-full text-center border-t border-gray-200">
+      <p className="text-[10px] mt-3">Laize Ferraz Design - 2023</p>
+      <p className="text-[10px]">@All rights reserved</p>
+    </footer>
+    )
+}
