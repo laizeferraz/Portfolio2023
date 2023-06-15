546fa4f28c90dc11eb3998fc50c8f34cc18c79f9
Laize Ferraz <laizeferraz@gmail.com>
2023-06-15 20:12:43 +1000

    hide email from bots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client"
 import { Animation } from '@/components/Animation'
 import ProjectsList from '@/components/ProjectsList'
 import Link from 'next/link'
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  const encEmail = "bGFpemVmZXJyYXpAZ21haWwuY29t";
+  useEffect(() => {
+    const form = document.getElementById("email");
+    form?.setAttribute("href", "mailto:".concat(atob(encEmail)));
+  }, [encEmail]);
   return (
     <main className='px-5'>
       <div className="mt-12">
@@ -16,6 +24,10 @@ export default function Home() {
         <ProjectsList />
         <div className="mt-5">
           <Link href="/projects">Check my work →</Link>
+        </div>
+        <div className='mt-10'>
+          <h2>Get in touch</h2>
+          <p id='contact' className='mt-5'>You can find me at various places around the web, including <a href="https://www.linkedin.com/in/laize-ferraz/"><span className='text-blue-500'>LinkedIn</span></a> and <a href="https://github.com/laizeferraz"><span className='text-indigo-500'>Github</span></a>, or send me an <a id='email' href=""><span className='text-yellow-600'>Email</span>.</a></p>
         </div>
       </div>
     </main>
