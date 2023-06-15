079a3dbf001e2306a730d3bd7057299569bceb06
Laize Ferraz <laizeferraz@gmail.com>
2023-06-15 18:14:00 +1000

    hide social in small screens

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,7 +3,7 @@ import { Github, Linkedin } from "lucide-react"
 export const Social = () => {
     return (
         <>
-            <div className="group fixed z-50 top-[139px] right-[210px] bg-gray-200 rounded-full p-2">
+            <div className="hidden group fixed z-50 top-[139px] lg:right-[20px] bg-gray-200 rounded-full p-2 lg:block">
                 <a href='https://www.linkedin.com/in/laize-ferraz/' target="_blank"
                     rel="noopener noreferrer"><Linkedin className='mb-5' /></a>
                 <a href='https://github.com/laizeferraz' target="_blank"
