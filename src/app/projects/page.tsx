6050a41ef18b0d66c3a97d073dfac7652bb56e0a
Laize Ferraz <laizeferraz@gmail.com>
2023-06-21 19:57:14 +1000

    add a title

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,8 +2,11 @@ import ProjectsList from '@/components/ProjectsList'
 
 export default function Work() {
   return (
-    <div className="h-screen">
-      <ProjectsList />
+    <div className="mt-10 h-screen">
+      <div className="mb-10">
+        <h2>Projects</h2>
+        <ProjectsList />
+      </div>
     </div>
   )
 }
