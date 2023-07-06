ac05a79db61a5b84ea56efbf9b07776fa61a42b1
Laize Ferraz <laizeferraz@gmail.com>
2023-07-06 19:00:44 +1000

    add new project and fix layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ import ProjectsList from '@/components/ProjectsList'
 
 export default function Work() {
   return (
-    <div className="mt-10 h-screen">
+    <div className="mt-10">
       <div className="mb-10">
         <h2>Projects</h2>
         <ProjectsList />
--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -6,10 +6,10 @@ export default function ProjectsList() {
   return (
     <div>
       <h4 className="mt-10">Work</h4>
-      <div className="md:grid md:grid-cols-2 md:gap-4">
+      <div className="md:gap-4 xl:flex xl:flex-row">
         {projectsList.projects.work.map((project) => (
           <div
-            className="mt-5 grid grid-cols-1 gap-4 lg:grid-cols-2"
+            className="mt-5 flex flex-col gap-4 md:max-w-xl lg:max-w-2xl lg:flex-row"
             key={project.id}
           >
             <Image
@@ -19,7 +19,7 @@ export default function ProjectsList() {
               height={400}
               className="rounded object-cover"
             />
-            <div>
+            <div className="flex flex-col justify-start">
               <h3>{project.Title}</h3>
               <p className="my-4 line-clamp-3">{project.Intro}</p>
               <Link href={`/projects/${project.id}`}>Read more →</Link>
@@ -28,11 +28,11 @@ export default function ProjectsList() {
         ))}
       </div>
       <h4 className="mt-10">Personal</h4>
-      <div className="md:grid md:grid-cols-2 md:gap-4">
+      <div className="md:gap-4 lg:flex-row xl:flex">
         {projectsList.projects.personal.map((project) => (
           <div
             key={project.id}
-            className="mt-5 grid grid-cols-1 gap-4 lg:grid-cols-2"
+            className="mt-5 flex flex-col gap-4 md:max-w-xl lg:max-w-2xl lg:flex-row"
           >
             <Image
               src={project.ImageIntro}
@@ -41,7 +41,7 @@ export default function ProjectsList() {
               height={400}
               className="rounded object-cover"
             />
-            <div>
+            <div className="flex flex-col justify-start">
               <h3>{project.Title}</h3>
               <p className="my-4 line-clamp-3">{project.Intro}</p>
               <Link href={`/projects/${project.id}`}>Read more →</Link>
