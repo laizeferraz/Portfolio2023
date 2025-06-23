35e42e600f19b733dc6b364b1df7d6ee7cfb5276
Laize Ferraz <laizeferraz@gmail.com>
2025-06-23 23:43:10 +1000

    update year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="mt-5 w-full border-t border-gray-200 text-center">
-      <p className="mt-3 text-[10px]">Laize Ferraz Design - 2024</p>
+      <p className="mt-3 text-[10px]">Laize Ferraz Design - 2025</p>
       <p className="text-[10px]">@All rights reserved</p>
     </footer>
   )
